2de5434211a0ac7b0deb195083a5f71a039d89aa
Daniel <daniel.olsson1992@gmail.com>
2019-10-22 13:43:02 +0200

    Comments for main file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+Main file for initialize the game and running it
+'''
+
 import arcade
 from Game import Game
 import os
@@ -8,13 +12,15 @@ SCREEN_TITLE = "NEAT ALGORITHM FOR A 2D PLATTFORM GAME"
 
 def main():
 
-	#Path to NEAT config file
+	#Get path to neat file .
 	local_dir = os.path.dirname(__file__)
 	config_path = os.path.join(os.path.dirname(__file__), 'config')
 
-	#Initalize and start game
+
 	print("To run new run type 'n', to continue on a saved run type 'c' and to run best genome from one saved run type 'b'")
 	choice_of_run = input();
+	
+	#Initalize and start game
 	game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 	game.setup_neat(config_path,choice_of_run )
 	game.setup()
